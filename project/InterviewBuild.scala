4549d15cd12bd69d3c298c3ff9bc16a68d1cb8db
Evgeny Zhulenev <evgeny.zhulenev@gmail.com>
2013-01-16 17:41:57 +0300

    Scala 2.10.0

--- project/InterviewBuild.scala
+++ project/InterviewBuild.scala
@@ -7,32 +7,27 @@ object InterviewBuild extends Build {
 
   lazy val buildSettings = Seq(
     organization := "interview",
-    version      := "0.1-SNAPSHOT",
-    scalaVersion := "2.9.2"
+    version := "0.1-SNAPSHOT",
+    scalaVersion := "2.10.0"
   )
-  
-  lazy val interview = Project(
-    id = "interview",
-    base = file("."),
-    settings = Project.defaultSettings ++ repositoriesSetting ++ Seq(libraryDependencies ++= Dependencies.root)
+
+  lazy val root = Project("interview", file("."),
+    settings = Project.defaultSettings ++ repositoriesSetting ++
+      Seq(libraryDependencies ++= Dependencies.root) ++
+      Seq(libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _))
   )
-  
+
   // -- Settings
 
   override lazy val settings = super.settings ++ buildSettings
 
   lazy val repositoriesSetting = Seq(
-    resolvers += "Sonatype Repository" at "http://oss.sonatype.org/content/groups/public/",
-    resolvers += "Scala Tools Repository" at "https://oss.sonatype.org/content/groups/scala-tools/",
-    resolvers += "JBoss repository" at "http://repository.jboss.org/nexus/content/repositories/",
-    resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
-    resolvers += "Typesafe Repository ide-2.9" at "http://repo.typesafe.com/typesafe/simple/ide-2.9/",
-    resolvers += "Twitter Repository" at "http://maven.twttr.com/",
-    resolvers += "Akka Repository" at "http://akka.io/snapshots/"
+    resolvers += "Sonatype Repository" at "http://oss.sonatype.org/content/groups/public/"
   )
-  }
+}
 
 object Dependencies {
+
   import Dependency._
 
   val root = Seq(slf4jApi, logback, scalaz) ++ Seq(Test.scalatest, Test.mockito)
@@ -43,19 +38,19 @@ object Dependency {
   // Versions
 
   object V {
-    val Slf4j        = "1.6.4"
-    val Mockito      = "1.9.0"
-    val Scalaz       = "6.0.4"
-    val Logback      = "1.0.0"    
-	
-	  val Scalatest    = "1.8"
+    val Slf4j = "1.6.4"
+    val Mockito = "1.9.0"
+    val Scalaz = "6.0.4"
+    val Logback = "1.0.0"
+
+    val Scalatest = "1.9.1"
   }
 
   // Compile
 
-  val slf4jApi          = "org.slf4j"                         % "slf4j-api"              % V.Slf4j
-  val logback           = "ch.qos.logback"                    % "logback-classic"        % V.Logback
-  val scalaz            = "org.scalaz"                       %% "scalaz-core"            % V.Scalaz
+  val slf4jApi = "org.slf4j" % "slf4j-api" % V.Slf4j
+  val logback = "ch.qos.logback" % "logback-classic" % V.Logback
+  val scalaz = "org.scalaz" %% "scalaz-core" % V.Scalaz
 
   // Provided
 
@@ -72,7 +67,8 @@ object Dependency {
   // Test
 
   object Test {
-    val mockito        = "org.mockito"                 % "mockito-all"             % V.Mockito      % "test" // MIT
-    val scalatest      = "org.scalatest"              %% "scalatest"               % V.Scalatest    % "test"
+    val mockito = "org.mockito" % "mockito-all" % V.Mockito % "test"
+    // MIT
+    val scalatest = "org.scalatest" %% "scalatest" % V.Scalatest % "test"
   }
 }
