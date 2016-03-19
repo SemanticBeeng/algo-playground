9c03719a091013832d6f34dbe704ed9e46b2a917
Eugene Zhulenev <ezhulenev@collective.com>
2016-03-19 00:13:35 -0400

    Counting subsets with given sum

--- src/main/scala/algoplayground/SubsetWIthGivenSum.scala
+++ src/main/scala/algoplayground/SubsetWIthGivenSum.scala
@@ -32,6 +32,22 @@ object SubsetWIthGivenSum extends App {
     (0 until ARR.length).flatMap(i => run(ARR.drop(i), M, SUM)).toSeq
   }
 
-  println(subset(Seq(3, 3, 3, 4, 4, 1, 0, 9, 0), 3, 9))
+  def countSubset(ARR: Seq[Int], M: Int, SUM: Int): Int = {
+    if (M > ARR.length) return 0
+
+    val memo = mutable.Map.empty[(Seq[Int], Int, Int), Int]
+
+    def run(arr: Seq[Int], m: Int, sum: Int): Int = memo.getOrElseUpdate((arr, m, sum), {
+      if (arr.isEmpty) return 0
+      if (m == 1 && sum == arr.head) return 1
+      if (m == 1 && sum != arr.head) return 0
+      (1 until arr.length).map(i => run(arr.drop(i), m - 1, sum - arr.head)).sum
+    })
+
+    (0 until ARR.length).map(i => run(ARR.drop(i), M, SUM)).sum
+  }
+
+  println(subset(Seq(3, 3, 3, 4, 4, 1, 0, 9, 0, 7), 3, 9))
+  println(countSubset(Seq(3, 3, 3, 4, 4, 1, 0, 9, 0, 7), 3, 9))
 
 }
